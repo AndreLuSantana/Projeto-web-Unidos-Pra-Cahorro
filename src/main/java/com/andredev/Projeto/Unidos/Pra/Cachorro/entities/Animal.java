a60ec4ca7f8826268e49d9e2c3689bb3fb3863b7
Andre Santana <andrelusantana@hotmail.com>
2021-07-05 23:58:49 -0300

    Atributos e metodos entidade Animal

--- src/main/java/com/andredev/Projeto/Unidos/Pra/Cachorro/entities/Animal.java
+++ src/main/java/com/andredev/Projeto/Unidos/Pra/Cachorro/entities/Animal.java
@@ -1,5 +1,110 @@
 package com.andredev.Projeto.Unidos.Pra.Cachorro.entities;
 
-public class Animal {
+import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.andredev.Projeto.Unidos.Pra.Cachorro.enuns.Opcoes;
+
+@Entity
+@Table(name = "tb_animal")
+public class Animal implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String identificador;
+	private Opcoes vacinado;
+	private Opcoes castrado;
+	private Opcoes retornadoParaRua;
+	
+	public Animal() {
+	}
+
+	public Animal(Long id, String identificador, Opcoes vacinado, Opcoes castrado, Opcoes retornadoParaRua) {
+		this.id = id;
+		this.identificador = identificador;
+		this.vacinado = vacinado;
+		this.castrado = castrado;
+		this.retornadoParaRua = retornadoParaRua;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getIdentificador() {
+		return identificador;
+	}
+
+	public void setIdentificador(String identificador) {
+		this.identificador = identificador;
+	}
+
+	public Opcoes getVacinado() {
+		return vacinado;
+	}
+
+	public void setVacinado(Opcoes vacinado) {
+		this.vacinado = vacinado;
+	}
+
+	public Opcoes getCastrado() {
+		return castrado;
+	}
+
+	public void setCastrado(Opcoes castrado) {
+		this.castrado = castrado;
+	}
+
+	public Opcoes getRetornadoParaRua() {
+		return retornadoParaRua;
+	}
+
+	public void setRetornadoParaRua(Opcoes retornadoParaRua) {
+		this.retornadoParaRua = retornadoParaRua;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Animal other = (Animal) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Animal [identificador=" + identificador + ", vacinado=" + vacinado + ", castrado=" + castrado
+				+ ", retornadoParaRua=" + retornadoParaRua + "]";
+	}
+	
 }
